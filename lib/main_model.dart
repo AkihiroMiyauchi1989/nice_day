c07376d688d970f5734bd47579c594939b3f8d93
宮内章宏 <myucakhr@gmail.com>
2020-11-14 01:36:43 +0900

    値を追加

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -4,25 +4,34 @@ import 'package:nice_day/day.dart';
 
 class MainModel extends ChangeNotifier {
   List<Day> dayList = [];
+  String newDayText = '';
 
-  Future getNiceDay() async {
+  Future getTodoList() async {
     final snapshot =
-    await FirebaseFirestore.instance.collection('dayList').get();
+      await FirebaseFirestore.instance.collection('dayList').get();
     final docs = snapshot.docs;
     final dayList = docs.map((doc) => Day(doc)).toList();
     this.dayList = dayList;
     notifyListeners();
   }
 
-  void getDayListRealtime(){
+  void getTodoListRealtime() {
     final snapshots =
-        FirebaseFirestore.instance.collection('dayList').snapshots();
-    snapshots.listen((snapshot){
+    FirebaseFirestore.instance.collection('dayList').snapshots();
+    snapshots.listen((snapshot) {
       final docs = snapshot.docs;
       final dayList = docs.map((doc) => Day(doc)).toList();
-      dayList.sort((a,b) => b.createdAt.compareTo(a.createdAt));
+      dayList.sort((a, b) => b.createdAt.compareTo(a.createdAt));
       this.dayList = dayList;
       notifyListeners();
+    });
+  }
+
+  Future add() async {
+    final collection = FirebaseFirestore.instance.collection('dayList');
+    await collection.add({
+      'title': newDayText,
+      'createdAt': Timestamp.now(),
     });
   }
 }
